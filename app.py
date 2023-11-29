4cdfccea9d26bf24be4185c074824d6dba5a8c30
tripincloud <redataleb568@gmail.com>
2023-11-29 14:38:21 +0100

    Create app.py

---
+++ app.py
@@ -0,0 +1,23 @@
+import streamlit as st
+from pages import Chat, Transcribe_Youtube
+
+# App configuration
+st.set_page_config(
+    page_title='AI-VideoQA',
+    page_icon='✨'
+)
+
+# Navigation
+PAGES = {
+    "Home" : 0_🌐_Home
+    "Upload Video": Transcribe_Youtube,
+    "Chat": Chat
+}
+
+st.sidebar.title('Navigation')
+selection = st.sidebar.radio("Go to", list(PAGES.keys()))
+
+# Display the selected page
+page = PAGES[selection]
+page.app()
+
